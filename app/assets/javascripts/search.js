837c3242b4cf48cc41b5c342baf72487acebd6ab
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2016-10-19 08:08:00 -0400

    remove search bar toggle feature

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,19 +1,6 @@
 $(function() {
-  // hideSearchInput();
-  searchFormToggle();
   submitSearch();
 });
-
-function hideSearchInput() {
-  $("[data-search='input']").hide()
-}
-
-function searchFormToggle() {
-  $("[data-search='icon']").on('click', function(e) {
-    console.log('in click function')
-    $("[data-search='input']").toggle('slow')
-  })
-}
 
 function submitSearch() {
   $("[data-search='input']").keydown(function(event) {
@@ -28,15 +15,3 @@ function submitSearch() {
      }
   });
 }
-
-// event.preventDefault();
-//   event.stopPropagation();
-//   $.ajax({
-//     url: 'http://localhost:3000/api/v1/songs', 
-//     method: 'POST', 
-//     dataType: 'json',
-//     data: $("#new-song-form").serialize(), 
-//     success: function(response) {
-//       debugger;
-//     }
-//   })

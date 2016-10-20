3420a7a933245eb370527b32259460a8296d23b6
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2016-10-20 13:24:08 -0400

    use .includes on song queries in Song search methods to pre-load and cut down on N+1 queries

--- app/models/song.rb
+++ app/models/song.rb
@@ -6,7 +6,7 @@ class Song < ApplicationRecord
 
   def self.agnostic_search(term)
     Array.new.tap do |songs|
-      songs += Song.where('title ILIKE ?', "%#{term}%")
+      songs += self.search_by_title(term)
       songs += self.search_by_artist(term)
       songs +=  self.search_by_album(term)
       return songs
@@ -14,31 +14,31 @@ class Song < ApplicationRecord
   end
 
   def self.search_by_title(term)
-    Song.where('title ILIKE ?', "%#{term}%")
+    Song.where('songs.title ILIKE ?', "%#{term}%").includes(:artist, :album)
   end
 
   def self.search_by_artist(term)
     # SELECT * FROM SONGS INNER JOIN artists ON artists.id = songs.artist_id WHERE artists.name LIKE term
-    Song.joins(:artist).where('artists.name ILIKE?', "%#{term}%")
+    Song.joins(:artist).where('artists.name ILIKE?', "%#{term}%").includes(:artist, :album)
   end
 
   def self.search_by_album(term)
-    Song.joins(:album).where('albums.title ILIKE?', "%#{term}%")
+    Song.joins(:album).where('albums.title ILIKE?', "%#{term}%").includes(:artist, :album)
   end
 
   def self.search_by_title_and_artist(title, artist)
-    Song.where('title ILIKE ?', "%#{title}%").joins(:artist).where('artists.name ILIKE?', "%#{artist}%")
+    Song.where('songs.title ILIKE ?', "%#{title}%").joins(:artist).where('artists.name ILIKE?', "%#{artist}%").includes(:artist, :album)
   end
 
   def self.search_by_title_and_album(title, album)
-    Song.where('songs.title ILIKE ?', "%#{title}%").joins(:album).where('albums.title ILIKE?', "%#{album}%")
+    Song.where('songs.title ILIKE ?', "%#{title}%").joins(:album).where('albums.title ILIKE?', "%#{album}%").includes(:artist, :album)
   end
 
   def self.search_by_title_and_artist_and_album(title, artist, album)
-    Song.where('songs.title ILIKE ?', "%#{title}%").joins(:artist).where('artists.name ILIKE?', "%#{artist}%").joins(:album).where('albums.title ILIKE?', "%#{album}%")
+    Song.where('songs.title ILIKE ?', "%#{title}%").joins(:artist).where('artists.name ILIKE?', "%#{artist}%").joins(:album).where('albums.title ILIKE?', "%#{album}%").includes(:artist, :album)
   end
 
   def self.search_by_artist_and_album(artist, album)
-    Song.joins(:artist).where('artists.name ILIKE?', "%#{artist}%").joins(:album).where('albums.title ILIKE?', "%#{album}%")
+    Song.joins(:artist).where('artists.name ILIKE?', "%#{artist}%").joins(:album).where('albums.title ILIKE?', "%#{album}%").includes(:artist, :album)
   end
 end
